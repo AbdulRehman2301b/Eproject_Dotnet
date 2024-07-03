09e993316c4f9e071dd5d6906f81f6c081bde273
Abdul-Rehman Mughal <139339915+AbdulRehman2301b@users.noreply.github.com>
2024-07-03 12:53:02 +0500

    first commit

---
+++ Controllers/WebController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SymphonyLtd.Controllers
+{
+    public class WebController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult about()
+        {
+            return View();
+        }
+        public IActionResult courses()
+        {
+            return View();
+        }
+        public IActionResult exam()
+        {
+            return View();
+        }
+        public IActionResult faqs()
+        {
+            return View();
+        }
+        public IActionResult contact()
+        {
+            return View();
+        }
+    }
+}
